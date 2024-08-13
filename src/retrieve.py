8ac1e94b7b4d444b1e04e179ee47d264d32285d9
arq0n <71539626+Arq0n@users.noreply.github.com>
2024-08-13 17:14:14 +0800

    Add error handling to retrieve.py

--- src/retrieve.py
+++ src/retrieve.py
@@ -1,19 +1,36 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
 
+CUISINES = ['american', 'asian', 'british', 'caribbean', 'central europe', 'chinese',
+            'eastern europe', 'french', 'greek', 'indian', 'italian', 'japanese', 'korean',
+              'kosher', 'mediterranean', 'mexican', 'middle eastern', 'nordic', 
+              'south american','south east asian', 'world']
+MEAL_TYPE = ['breakfast', 'brunch', 'lunch' , 'dinner', 'snack', 'teatime']
+DIET_LABELS = ['balanced', 'high-fiber', 'high-protein', 'low-carb', 'low-fat', 'low-sodium']
+
+load_dotenv()
+EDAMAM_API_KEY = os.getenv('EDAMAM_API_KEY')
 
 def connect(text, cuisine, meal_type, diet, allergies=None):
+    if cuisine not in CUISINES:
+        raise ValueError('Not a valid cuisine type')
+    if meal_type not in MEAL_TYPE:
+        raise ValueError('Not a valid meal time')
+    if diet not in DIET_LABELS:
+        raise ValueError('Not a valid diet label')
+    
     url = 'https://api.edamam.com/api/recipes/v2'
     params = {
         'type': 'any',
         'app_id': '04537fa2',
-        'app_key': '229288441e21574ad74579f88e34b687',
+        'app_key': EDAMAM_API_KEY,
         'q': text,
         'cuisineType': cuisine,
         'mealType': meal_type,
         'diet': diet}
-    headers = {
-            'Accept-Language' : 'en'}
+    headers = {'Accept-Language' : 'en'}
 
     response = requests.get(url, params=params, headers=headers)
     data = response.content.decode(encoding='utf-8')
