69fc9d39a164f4cc10ca72dfc379af8324db7c24
arq0n <71539626+Arq0n@users.noreply.github.com>
2024-08-19 22:15:04 +0800

    Add docstrings to methods

--- src/recipies.py
+++ src/recipies.py
@@ -8,14 +8,27 @@ class Recipe:
         self._meal_type = json_data['mealType']
 
     def __str__(self):
-        return ' '.join(self._cuisine) + ' acuisine: ' + self._name + ' for ' + \
+        """
+        Representation of the class that displays cuisine type, dish name, 
+        and when to eat the dish
+        """
+        return ' '.join(self._cuisine) + ' cuisine: ' + self._name + ' for ' + \
             ' '.join(self._meal_type)
     
     def get_health_labels(self) -> list[str]:
+        """
+        Return health labels of the dish in a list of strings
+        """
         return self._health_labels
     
     def get_cautions(self) -> list[str]:
+        """
+        Return cautions of the dish in a list of strings
+        """
         return self._cautions
     
     def get_ingredients(self) -> list[str]:
+        """
+        Returns list of ingredients needed for the dish
+        """
         return self._ingredients
