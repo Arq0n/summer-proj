8ba8b956c1f254cfc65f11b1e781a2a087b9aa1b
arq0n <71539626+Arq0n@users.noreply.github.com>
2024-08-19 22:16:35 +0800

    Ask for user input

--- src/main.py
+++ src/main.py
@@ -2,9 +2,30 @@ import retrieve
 import json
 from recipies import Recipe
 
-j = dict()
-with open('data.json', 'r') as infile:
-    j = json.load(infile)
+ingredients = input("Enter main ingredients: ")
+
+meal_type = input("Enter meal type (breakfast, lunch, etc.): ")
+while meal_type not in retrieve.MEAL_TYPE:
+    print('Not a valid meal time')
+    meal_type = input("Enter meal type (breakfast, lunch, etc.): ")
+
+cuisine = input("Enter cuisine type or leave empty: ")
+cuisine = None if len(cuisine) == 0 else cuisine
+
+diet = input("Enter type of diet or leave empty: ")
+diet = None if len(diet) == 0 else diet
+
+data = None
+if cuisine is None and data is None:
+    data = retrieve.connect(ingredients, meal_type)
+elif cuisine is None:
+    data = retrieve.connect(ingredients, meal_type, diet=diet)
+elif diet is None:
+    data = retrieve.connect(ingredients, meal_type, cuisine=cuisine)
+else:
+    data = retrieve.connect(ingredients, meal_type, cuisine, diet)
+
+j = json.loads(data)
 
 for recipe in j['hits']:
     r = Recipe(recipe['recipe'])
